0731dfc50fa76867ce2f4475136d3e44ffd1ef56
Ingrid Queiroz <queirozingrid.c@gmail.com>
2023-07-31 19:10:23 -0300

    Add two new functions to Update and Delete Transactions

--- database/db_queries.go
+++ database/db_queries.go
@@ -6,12 +6,12 @@ import (
 	"wallet-management/types"
 )
 
-func CreateTransaction(db *sql.DB, transaction types.Transaction) error {
+func CreateTransaction(db *sql.DB, transaction types.Transaction) (err error) {
 
 	query := fmt.Sprintf("INSERT INTO transactions"+
 		"(amount, description, transaction_type)"+
 		"VALUES (%+v, %+v, %+v)", transaction.Amount, transaction.Description, transaction.Type)
-	_, err := db.Query(query)
+	_, err = db.Query(query)
 	if err != nil {
 		return (err)
 	}
@@ -35,4 +35,27 @@ func GetAllTransactions(db *sql.DB) (transactions []types.Transaction, err error
 	}
 
 	return transactions, nil
+}
+
+func UpdateTransaction(db *sql.DB, transaction types.Transaction) (err error) {
+	query := fmt.Sprintf("UPDATE transactions "+
+		"SET amount=%+v, description=%+v, transaction_type=%+v "+
+		"WHERE id=%+v", transaction.Amount, transaction.Description, transaction.Type, transaction.Id)
+
+	_, err = db.Query(query)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DeleteTransactionById(db *sql.DB, id int) (err error) {
+	query := fmt.Sprintf("DELETE FROM transactions WHERE id=%+v", id)
+	_, err = db.Query(query)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -18,12 +18,59 @@ func main() {
 		Type:        types.INFLOW,
 	}
 
+	// CREATE A NEW TRANSACTION
+	fmt.Println("Creating transaction...")
 	err := database.CreateTransaction(db, transaction)
 	if err != nil {
 		panic(err)
 	}
 
+	// GET ALL TRANSACTIONS IN THE DB
+	fmt.Println("All transactions in the Database...")
 	transactions, err := database.GetAllTransactions(db)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, tr := range transactions {
+		fmt.Printf("---Transaction: %+v - %+v - %+v - %+v ---\n", tr.Id, tr.Amount, tr.Description, tr.Type)
+	}
+
+	updateTransaction := types.Transaction{
+		Id:          1,
+		Amount:      56.80,
+		Description: "'Something bad here'",
+		Type:        types.OUTFLOW,
+	}
+
+	// UPDATE THIS ONE TRANSACTION
+	fmt.Println("\nUpdating transaction...")
+	err = database.UpdateTransaction(db, updateTransaction)
+	if err != nil {
+		panic(err)
+	}
+
+	// GET ALL TRANSACTIONS IN THE DB
+	fmt.Println("All transactions in the Database...")
+	transactions, err = database.GetAllTransactions(db)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, tr := range transactions {
+		fmt.Printf("---Transaction: %+v - %+v - %+v - %+v ---\n", tr.Id, tr.Amount, tr.Description, tr.Type)
+	}
+
+	// DELETE THIS ONE TRANSACTION
+	fmt.Println("\nDeleting transaction...")
+	err = database.DeleteTransactionById(db, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	// GET ALL TRANSACTIONS IN THE DB
+	fmt.Println("All transactions in the Database...")
+	transactions, err = database.GetAllTransactions(db)
 	if err != nil {
 		panic(err)
 	}
